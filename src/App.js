1bf5e74c3913f44a0047b0fcf954dd27bb62a7ed
Ruben David <rupedaveco25@gmail.com>
2023-04-10 18:04:25 -0400

    Poninedo Estilo y el logo de freeCodeCamp

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
 import './App.css';
+import freCodeCamp from './imagenes/descarga.jfif';
 
 function App() {
   return (
     <div className="App">
-      
+      <div className='freecodecamp-logo-container'>
+        <img
+          className='freecodecamp-logo'
+          src={freCodeCamp}
+          alt='logo de freCodeCamp' 
+        />
+        <div className='contenedor-principal'>
+          
+        </div>
+      </div> 
     </div>
   );
 }
